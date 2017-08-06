ac15657f3675ec27e642d1be1e95598713c373c0
alan-maldonado <alan.maldonado@wizeline.com>
2017-08-06 18:25:34 -0500

    devtools redux

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import App from './components/App';
+import reducers from './reducers';
 
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(reducers, {}, composeWithDevTools(applyMiddleware()));
 
 ReactDOM.render(
   <Provider store={store}>
