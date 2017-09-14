fcd8a7137892492a353d70c59c660c934f49cf45
alan-maldonado <alan.maldonado@wizeline.com>
2017-09-13 22:25:34 -0500

    Added survey route

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,8 +1,24 @@
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 
+const Survey = mongoose.model('surveys');
+
 module.exports = app => {
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
-    res.send();
+  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(',')
+                            .map(email => ({ email: email.trim() })),
+      _user: req.user.id,
+      dateSent: Date.now()
+    });
+
+    // await survey.save();
+    res.send({ hi: 'there'});
   });
 };
