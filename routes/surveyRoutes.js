83b04c72278f45b8fb1621b71e642d9c633b37a1
alan-maldonado <alan.maldonado@wizeline.com>
2017-09-14 00:02:43 -0500

    send emails with sendgrid

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
+  app.get('/api/surveys/thanks', (req, res) => {
+    res.send('Thanks for voting!');
+  });
+
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
@@ -18,7 +24,16 @@ module.exports = app => {
       dateSent: Date.now()
     });
 
-    // await survey.save();
-    res.send({ hi: 'there'});
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+    try {
+      await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
+
+      res.send(user);
+    } catch (err) {
+      res.status(422).send(err);
+    }
   });
 };
