ac80f52d58df13de777d539c81750c42f44429a3
alan-maldonado <alan.maldonado@wizeline.com>
2017-09-15 11:22:04 -0500

    added redux-form

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 import Header from './Header';
 import Dashboard from './Dashboard';
-import SurveyNew from './SurveyNew';
 import Landing from './Landing';
-import * as actions from '../actions';
+import SurveyNew from './surveys/SurveyNew';
 
 class App extends Component {
-  componentDidMount() {
+  componentDidMount () {
     this.props.fetchUser();
   }
 
-  render() {
+  render () {
     return (
-      <div className="container">
+      <div className='container'>
         <BrowserRouter>
           <div>
             <Header />
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/surveys" component={Dashboard} />
-            <Route path="/surveys/new" component={SurveyNew} />
+            <Route exact path='/' component={Landing} />
+            <Route exact path='/surveys' component={Dashboard} />
+            <Route path='/surveys/new' component={SurveyNew} />
           </div>
         </BrowserRouter>
       </div>
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Dashboard extends Component {
   render () {
     return (<div>
       Dashboard
       <div className="fixed-action-btn">
-        <a className="btn-floating btn-large red">
+        <Link to="/surveys/new" className="btn-floating btn-large red">
           <i className="material-icons">add</i>
-        </a>
+        </Link>
       </div>
     </div>)
   }
