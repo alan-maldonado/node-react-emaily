7613847f8d1f7a74b56a21ed1d5c991869441948
alan-maldonado <alan.maldonado@wizeline.com>
2017-09-16 19:52:48 -0500

    survey form

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
+import _ from 'lodash';
 import SurveyField from './SurveyField';
+
+const FIELDS = [
+  { name: 'title', label: 'Survey Title' },
+  { name: 'subject', label: 'Subject Line' },
+  { name: 'body', label: 'Email Body' },
+  { name: 'emails', label: 'Recipient List' }
+];
 
 class SurveyForm extends Component {
   renderFields () {
-    return (
-      <div>
-        <Field
-          name='title'
-          type='text'
-          component={SurveyField}
-        />
-      </div>
-    );
+    return _.map(FIELDS, ({ label, name }) => (
+      <Field
+        key={name}
+        type='text'
+        name={name}
+        label={label}
+        component={SurveyField}
+      />
+    ));
   }
 
   render () {
@@ -20,8 +29,9 @@ class SurveyForm extends Component {
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
           {this.renderFields()}
-          <button className='btn' type='submit'>Submit
-            <i className='material-icons right'>send</i>
+          <Link to='/surveys' className='red btn-flat white-text'>Cancel</Link>
+          <button className='teal btn-flat right white-text' type='submit'>Next
+            <i className='material-icons right'>done</i>
           </button>
         </form>
       </div>
